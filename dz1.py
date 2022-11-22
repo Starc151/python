c1f89a1bb88cb33292563034cb58bba29623b0ab
Aleksey Pashkov <dr.Starc@ya.ru>
2022-11-22 16:35:43 +0300

    dz1 task4

--- dz1.py
+++ dz1.py
@@ -20,5 +20,22 @@ def task3():
     else:
         print(4)
 
+def task4():
+    numQuarter = int(input('введите номер четверти: '))
+    match numQuarter:
+        case 1: 
+            print('Диапазон: x>0, y>0')
+        case 2: 
+            print('Диапазон: x<0, y>0')
+        case 3: 
+            print('Диапазон: x<0, y<0')
+        case 4: 
+            print('Диапазон: x>0, y<0')
+        case _:
+            print('Такой четверти нет')
+
+
+
 # task1()
 # task3()
+task4()
