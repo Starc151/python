4819289f91a510d6d94bcb17b79c1bdc8a4df82f
Aleksey Pashkov <dr.Starc@ya.ru>
2022-11-27 18:02:03 +0300

    dz2 task4

--- dz2.py
+++ dz2.py
@@ -1,5 +1,6 @@
 import math
 import random
+
 def task1():
     sum = 0
     num = input('Введите число: ')
@@ -26,6 +27,25 @@ def task3():
     for i in range(1, num+1, 1):
         data.append((1+1/i)**i)
     print(data)
+    print(sum(data))
+
+def task4():
+    mult = 1
+    num = int(input('Введите число: '))
+    data = []
+    for i in range(-num, num+1, 1):
+        data.append(i)
+    print(f"Получившийся список: {data}")
+    f = open('forDZ2.txt', 'r').read().split('\n')
+    positionData = []
+    if int(f[-1]) >= len(data):
+        print("Введите число побольше!")
+        return False
+    for i in f:
+        mult *= data[int(i)]
+        positionData.append(i)
+    print(f"Список позиций из файла: {positionData}") # в фвйле forDZ2.txt
+    print(f"Произведение элементов на указанных позициях: {mult}")
 
 def task5():
     num = int(input('Введите число: '))
@@ -36,7 +56,10 @@ def task5():
     random.shuffle(data)
     print(data)
 
+
+
 # task1()
 # task2()
 # task3()
+task4()
 # task5()
