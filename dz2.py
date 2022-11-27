80b155acbb9fd96b25a29f014b3c8c85f6834627
Aleksey Pashkov <dr.Starc@ya.ru>
2022-11-27 12:53:03 +0300

    dz2 task3

--- dz2.py
+++ dz2.py
@@ -7,4 +7,14 @@ def task2():
     for i in range(1, num+1, 1):
         data.append(math.factorial(i))
     print(data)
-task2()
+
+def task3():
+    num = int(input('Введите число: '))
+    data = []
+    for i in range(1, num+1, 1):
+        data.append((1+1/i)**i)
+    print(data)
+
+
+# task2()
+# task3()
