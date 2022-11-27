9a3cd8c1653220648829097b08d2d17778c006c5
Aleksey Pashkov <dr.Starc@ya.ru>
2022-11-27 12:59:43 +0300

    dz 2 task5

--- dz2.py
+++ dz2.py
@@ -1,4 +1,5 @@
 import math
+import random
 
 
 def task2():
@@ -15,6 +16,14 @@ def task3():
         data.append((1+1/i)**i)
     print(data)
 
-
+def task5():
+    num = int(input('Введите число: '))
+    data = []
+    for i in range(-num, num+1, 1):
+        data.append(i)
+    print(data)
+    random.shuffle(data)
+    print(data)
 # task2()
 # task3()
+# task5()
