e8b2fa1ec6e6fce86237c147c071e91f60f932ef
Aleksey Pashkov <dr.Starc@ya.ru>
2022-11-29 15:25:58 +0300

    dz3 task3

--- dz3.py
+++ dz3.py
@@ -1,7 +1,7 @@
 import math
 import random
 def createList():
-    num = int(input("ВВедите чило элементов списка: "))
+    num = int(input("Введите чило элементов списка: "))
     data = []
     for i in range(0, num, 1):
         data.append(random.randint(0, 10))
@@ -23,5 +23,20 @@ def task2():
         data2.append(data[i]*data[-i-1])
     print(f"Произведения пар чисел исходного списка: {data2}")
 
+def task3():
+    # Решение без строк
+    data = createList()
+    dataFloat =[]
+    for i in range(len(data)):
+        data[i] = data[i] + random.random()
+        data[i] = round(data[i], 2)
+        dataFloat.append(data[i]%1)
+        dataFloat[i] = round(dataFloat[i], 2)
+    print(f"Случайный список с  дробной частью: {data}")
+    print(f"Список без целой части: {dataFloat}")
+    r = round(max(dataFloat) - min(dataFloat), 2)
+    print(f"Разница между макс и мин значением дробной части элементов: {r}")
+
 # task1()
 # task2()
+# task3()
