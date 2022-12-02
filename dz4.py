33e71d4c6d42cfd55a1b2433322cfc28e672b231
Aleksey Pashkov <dr.Starc@ya.ru>
2022-12-02 14:52:11 +0300

    dz4 task2

---
+++ dz4.py
@@ -0,0 +1,15 @@
+def task2():
+    num = int(input('Введите число для вычисления простых множителей: '))
+    endList = []
+    for i in range(2, num + 1):
+        if num % i == 0:
+            count = 1
+            for j in range(2, (i // 2 + 1)):
+                if(i % j == 0):
+                    count = 0
+                    break
+            if(count == 1):
+                endList.append(i)
+    print(endList)
+
+task2()
