e61669a89c529bd4227561af30da9dd5e1b1ee2a
Aleksey Pashkov <dr.Starc@ya.ru>
2022-12-12 13:37:35 +0300

    dz6

--- dz5.py
+++ dz5.py
@@ -9,10 +9,9 @@ game.title('Крестики и Нолики')
 game.geometry('440x440')
 
 firstMove = random.randint(0, 1)
-if firstMove == 0:
-    tkinter.messagebox.showinfo("", "Первым ходите вы")
-else:
-    tkinter.messagebox.showinfo("", "Первым ходит компьютер")
+statusGame = ['_']*9
+freePosition = list(range(0, 9))
+v = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
 
 mapGame = Canvas(game, width='390', height='390', bg='orange')
 mapGame.place(x=25, y=25)
@@ -25,12 +24,7 @@ for i in range(0, 9):
                                         fill = '#CCCCCC',
                                         activefill = '#FFFAFA')
 
-statusGame = ['_']*9
-freePosition = list(range(0, 9))
-v = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
-
 def victory(s):
-    print()
     for i in v:
         if s == statusGame[i[0]] == statusGame[i[1]] == statusGame[i[2]]:
             if s == 'x':
@@ -39,9 +33,9 @@ def victory(s):
             elif s == 'o':
                tkinter.messagebox.showinfo("", "Победа за компьютером")
                game.destroy()
-    # if statusGame.count('_') == False:
-    #     tkinter.messagebox.showinfo("", "Ничья")
-    #     game.destroy()
+    if statusGame.count('_') == False:
+        tkinter.messagebox.showinfo("", "Ничья")
+        game.destroy()
 
 def compMove():
     cp = random.choice(freePosition)
@@ -70,20 +64,9 @@ def compMove():
         elif statusGame[i[2]] == 'o' and (statusGame[i[0]] == statusGame[i[1]] == '_'):
             cp = i[1]
             break
-        elif statusGame[0] == '':
-            cp = 0
+        elif statusGame[i[1]] == 'o' and (statusGame[i[0]] == statusGame[i[2]] == '_'):
+            cp = i[0]
             break
-        elif statusGame[6] == '':
-            cp = 6
-            break
-        elif statusGame[2] == '':
-            cp = 3
-            break
-        elif statusGame[8] == '':
-            cp = 8
-            break
-        elif statusGame[4] == '_':
-            cp = 4
     col = cp % 3
     row = cp // 3
     x = 25 + 130 * col
@@ -92,9 +75,6 @@ def compMove():
     statusGame[cp]  = "o"
     victory('o')
     freePosition.remove(cp)
-
-if firstMove == 1:
-    compMove()
 
 def humanMove(event):
     col = event.x // 130
@@ -110,70 +90,10 @@ def humanMove(event):
         freePosition.remove(index)
         game.after(500, compMove)
 
-
-    
-
-
+if firstMove == 0:
+    tkinter.messagebox.showinfo("", "Первым ходите вы")
+else:
+    tkinter.messagebox.showinfo("", "Первым ходит компьютер")
+    compMove()
 mapGame.bind('<Button-1>', humanMove)
 game.mainloop()
-# def gameXO():
-#     mapping = ['_']*9
-#     freePosition = list(range(0, 9))
-#     move = random.randint(0,1)
-#     print("Карта игры:")
-#     printList("".join(map(str, list(range(1, 10)))))
-#     selectDifficulty()
-#     move1 = human
-#     move2 = comp
-#     if move == 1:
-#         print("Вы ходите первым")
-#     else:
-#        print("Первым ходит компьютер")
-#        move1, move2 = move2, move1
-
-#     for i in range (len(freePosition)):
-#         move1(mapping, freePosition)
-#         if len(freePosition) == 0:
-#             print("Ничья")
-#             sys.exit()
-#         move2(mapping, freePosition)
-
-# def selectDifficulty():
-#     global difficulty
-#     difficulty = 0
-#     difficulty = input('Выберите сложность от 0, 1 или 2: ')
-#     if difficulty.isalpha():
-#         print("Так играть компюьтер не сможет")
-#         return selectDifficulty()
-#     difficulty = int(difficulty)
-#     if 0 > difficulty or difficulty > 2:
-#         print("Так играть компюьтер не сможет")
-#         return selectDifficulty()
-
-# def comp(mapping, freePosition):
-#     print("Ход компьютера...")
-#     dlp = random.choice(freePosition)
-#     v = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
-#     random.shuffle(v)
-#     if difficulty == 2:
-#         for i in v:
-#             if mapping[i[0]] == mapping[i[1]] == 'x' and mapping[i[2]] == '_':
-#                 dlp = i[2]
-#             elif mapping[i[0]] == mapping[i[2]] == 'x' and mapping[i[1]] == '_':
-#                 dlp = i[1]
-#             elif mapping[i[1]] == mapping[i[2]] == 'x' and mapping[i[0]] == '_':
-#                 dlp = i[0]
-#     if difficulty > 1:
-#         for i in v:
-#             if mapping[i[0]] == mapping[i[1]] == 'o' and mapping[i[2]] == '_':
-#                 dlp = i[2]
-#             elif mapping[i[0]] == mapping[i[2]] == 'o' and mapping[i[1]] == '_':
-#                 dlp = i[1]
-#             elif mapping[i[1]] == mapping[i[2]] == 'o' and mapping[i[0]] == '_':
-#                 dlp = i[0]
-#     mapping[dlp] = 'o'
-#     freePosition.remove(dlp)
-#     printList(mapping)
-#     victory(mapping, 'o')
-
-# gameXO()
